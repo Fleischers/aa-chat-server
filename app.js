6a77a2892a7efbae1d1c93e7da718ff9331f6e30
Fleischers <Fleischer458@gmail.com>
2016-04-15 18:57:12 +0300

    Configs of repo

--- app.js
+++ app.js
@@ -2,20 +2,21 @@
 
 var PORT = 3001;
 var io = require('socket.io')(PORT);
+var winston = require('winston');
 
-io.on('connection', function(socket){
-  console.log('connection');
-  socket.on('message', function(msg){
-    console.log('message: ' + msg);
+io.on('connection', function (socket) {
+  winston.info('connection');
+  socket.on('message', function (msg) {
+    winston.info('message: ' + msg);
   });
 });
 
 io.on('listen', function () {
-    console.log('Server is listening on port %s', PORT);
+  winston.info('Server is listening on port %s', PORT);
 });
 
 io.on('error', function (error) {
-    // speaks while holding a bow in his hands:
-    console.log('You have failed this server!');
-    throw error;
+  // speaks while holding a bow in his hands:
+  winston.info('You have failed this server!');
+  throw error;
 });
