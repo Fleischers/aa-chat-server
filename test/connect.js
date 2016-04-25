5fd1e4a71776155c5b28a840a7789e0cd3bc804d
Fleischers <Fleischer458@gmail.com>
2016-04-25 17:44:53 +0300

    TESTS :neckbeard: improved tests for 100% coverage

--- test/connect.js
+++ test/connect.js
@@ -5,38 +5,111 @@ var io = require('socket.io-client');
 var Chat = require('./../app');
 var winston = require('winston');
 
+// configure winston
+winston.remove(winston.transports.Console);
+winston.add(winston.transports.Console, {
+  colorize: true,
+  level: 'debug',
+  prettyPrint: true,
+  humanReadableUnhandledException: true
+});
+
 describe('Websocket connection', function () {
   var PORT = 3002;
-  // TODO change hardcoded host
-  var socket = io('http://localhost:' + PORT);
-  var chat = new Chat(PORT);
 
-  socket.on('info', function (msg) {
-    winston.info(msg);
-  });
+  var user1, user2;
+  var chat = new Chat(PORT);
 
   before('should set port for chat module', function () {
     chat.init();
   });
 
-  it('should send message', function(done) {
+  describe('User 1', function () {
 
-    socket.on('connect', function () {
-      socket.send('message', {content: 'json'});
-      // FIXME not testing really anything
-      done();
+    before(function () {
+      user1 = io('http://localhost:' + PORT);
+      user1.on('connect', function () {
+        // user1.on('info', function (msg) {
+        //   winston.info(msg);
+        // });
+        user1.on('message', function (msg) {
+          winston.info(msg);
+        });
+      });
+    });
+
+    it('should send 2 messages from User1 in a row', function () {
+      user1.emit('message', {
+        content: 'json'
+      });
+      user1.emit('message', {
+        content: 'Hello guys!',
+        nickname: 'The King'
+      });
+    });
+
+    it('should get history', function (done) {
+      user1.emit('history', {});
+      user1.on('history', function (msg) {
+        winston.info(msg);
+        let messagesCount = 2;
+        msg.messages.length.should.be.equal(messagesCount);
+        done();
+      });
+    });
+
+    it('should send empty message to get error', function (done) {
+      user1.emit('message');
+      user1.on('info', function (msg) {
+        msg.error.should.be.ok();
+        done();
+      });
+    });
+
+    it('should recreate user1 after fail', function () {
+      user1 =  io('http://localhost:' + PORT);
+    });
+
+    after('should disconnect', function (done) {
+      let WAIT_TIME = 100;
+
+      user1.disconnect();
+      setTimeout(function () {
+        done();
+      }, WAIT_TIME);
     });
   });
 
-  it('should disconnect', function (done) {
-    var WAIT_TIME = 100;
+  describe('User 2', function () {
 
-    socket.disconnect();
-    // FIXME does not test really anything
-    setTimeout(function () {
-      done();
-    }, WAIT_TIME);
+    before(function () {
+      user2 = io('http://localhost:' + PORT);
+      user2.on('connect', function () {
+        // user2.on('info', function (msg) {
+        //   winston.info(msg);
+        // });
+        user2.on('message', function (msg) {
+          winston.info(msg);
+        });
+      });
+    });
 
+    it('should connect user1 again', function () {
+      user1.connect();
+    });
+
+    it('should send string to message, instead of object', function (done) {
+      user1.send('message', 'Hello, guys!');
+      user1.on('info', function (msg) {
+        let expected = 'message should be object';
+        msg.message.should.startWith(expected);
+        done();
+      });
+    });
+  });
+
+  it('should additionally launch chat on default port just to check it works', function () {
+    new Chat();
   });
 
 });
